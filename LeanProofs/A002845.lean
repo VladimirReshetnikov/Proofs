1d748834592352797202491a1d869f885c1ff4f7
Vladimir Reshetnikov <v.reshetnikov@gmail.com>
2026-07-06 10:19:36 -0700

    Prove A002845(10) in Lean

--- LeanProofs/A002845.lean
+++ LeanProofs/A002845.lean
@@ -188,6 +188,10 @@ theorem a002845_eight : a002845 8 = 36 := by
 theorem a002845_nine : a002845 9 = 78 := by
   native_decide
 
+/-- `A002845(10) = 171`. -/
+theorem a002845_ten : a002845 10 = 171 := by
+  native_decide
+
 end A002845
 
 end LeanProofs
